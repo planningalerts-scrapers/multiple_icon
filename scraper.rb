969a47449fef772437878c7f69ac9d23b612ec6e
Matthew Landauer <mlandauer@gmail.com>
2019-07-01 15:18:49 +1000

    Retry authories that fail at the end

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,39 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-exceptions = []
-IconScraper::AUTHORITIES.keys.each do |authority_label, url|
-  puts "\nCollecting feed data for #{authority_label}..."
+def scrape(authorities)
+  exceptions = {}
+  authorities.each do |authority_label, url|
+    puts "\nCollecting feed data for #{authority_label}..."
 
-  begin
-    IconScraper.scrape(authority_label) do |record|
-      record["authority_label"] = authority_label.to_s
-      IconScraper.log(record)
-      ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+    begin
+      IconScraper.scrape(authority_label) do |record|
+        record["authority_label"] = authority_label.to_s
+        IconScraper.log(record)
+        ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+      end
+    rescue StandardError => e
+      STDERR.puts "#{authority_label}: ERROR: #{e}"
+      STDERR.puts e.backtrace
+      exceptions[authority_label] = e
     end
-  rescue StandardError => e
-    STDERR.puts "#{authority_label}: ERROR: #{e}"
-    STDERR.puts e.backtrace
-    exceptions << e
   end
+  exceptions
+end
+
+authorities = IconScraper::AUTHORITIES.keys
+puts "Scraping authorities: #{authorities.join(', ')}"
+
+exceptions = scrape(authorities)
+
+unless exceptions.empty?
+  puts "\n***************************************************"
+  puts "Now retrying authorities which earlier had failures"
+  puts "***************************************************"
+
+  exceptions = scrape(exceptions.keys)
 end
 
 unless exceptions.empty?
-  raise "There were earlier errors. See output for details"
+  raise "There were errors with the following authorities: #{exceptions.keys}. See earlier output for details"
 end
